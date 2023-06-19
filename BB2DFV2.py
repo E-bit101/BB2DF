efcf91062db5bc64749240015853e24fe5c0f52d
slime89 <73862786+E-bit101@users.noreply.github.com>
2023-06-19 09:09:51 -0800

    Update BB2DFV2.py
    bugfix

--- BB2DFV2.py
+++ BB2DFV2.py
@@ -1,5 +1,13 @@
 import json
-
+def importmodules():
+    try:
+        import pygame as pg
+        import keyboard as kb
+    except:
+        input("Modules failed to import. Please run the following commands in your console: [pip install pygame] and [pip install keyboard]. Press enter to continue.")
+        importmodules()
+importmodules()
+print("Better UI coming soon!")
 def parse_json(json_str, scale):
     data = json.loads(json_str)
     elements = data['elements']
@@ -14,19 +22,19 @@ def parse_json(json_str, scale):
         z_to -= z_from
         xrotation, yrotation, zrotation = e.get('rotation', [0, 0, 0])
         xorigin, yorigin, zorigin = e['origin']
-        xrotation = round(xrotation * scale,3) 
-        yrotation = round(yrotation * scale,3) 
-        zrotation = round(zrotation * scale,3) 
-        x_from = round(x_from * scale,3) 
-        y_from = round(y_from * scale,3) 
-        z_from = round(z_from * scale,3) 
-        x_to = round(x_to * scale,3) 
-        y_to = round(y_to * scale,3) 
-        z_to= round(z_to * scale,3) 
+        xrotation = round(xrotation * scale,3)
+        yrotation = round(yrotation * scale,3)
+        zrotation = round(zrotation * scale,3)
+        x_from = round(x_from * scale,3)
+        y_from = round(y_from * scale,3)
+        z_from = round(z_from * scale,3)
+        x_to = round(x_to * scale,3)
+        y_to = round(y_to * scale,3)
+        z_to= round(z_to * scale,3)
         object = str(x_from) + "/" + str(y_from) + "/" + str(z_from) + "/" + str(xrotation) + "/" + str(yrotation) + "/" + str(zrotation) + "/" + str(x_to) + "/" + str(y_to) + "/" + str(z_to) + "/" + name
         result.append(object)
     return result
 
 jsoninput = input("Code here")
 scale = float(input("Scale factor"))
-input("~".join(parse_json(jsoninput,scale)))
+input("(newline)".join(parse_json(jsoninput,scale)))
